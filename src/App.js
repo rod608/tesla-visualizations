146af5824df9c9b39a7b5895ffabd61aa165878a
rod608 <rod608ney@gmail.com>
2024-12-13 22:23:25 -0500

    add title

--- src/App.js
+++ src/App.js
@@ -9,22 +9,26 @@ import WheelSalesPieChart from "./components/WheelSalesPieChart";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div>
-          <h2>Tesla Sales per Day</h2>
-          <TeslaSalesBarChart data={dataset} />
-        </div>
+      <>
+        <h1>August 2022 Tesla Sales Dashboard</h1>
 
-        <div>
-          <h2>Model Proportion</h2>
-          <ModelSalesPieChart data={dataset} />
-        </div>
+        <div className="App">
+          <div>
+            <h2>Tesla Sales per Day</h2>
+            <TeslaSalesBarChart data={dataset} />
+          </div>
 
-        <div>
-          <h2>Wheels Proportion</h2>
-          <WheelSalesPieChart data={dataset} />
+          <div>
+            <h2>Model Proportion</h2>
+            <ModelSalesPieChart data={dataset} />
+          </div>
+
+          <div>
+            <h2>Wheels Proportion</h2>
+            <WheelSalesPieChart data={dataset} />
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }
