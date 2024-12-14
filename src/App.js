ba32dc01006fc1c97c65eba4db4ee6902ffbeb27
rod608 <rod608ney@gmail.com>
2024-12-13 19:28:17 -0500

    formatted via prettier

--- src/App.js
+++ src/App.js
@@ -8,38 +8,43 @@ import TeslaSalesPieChart from "./components/TeslaSalesPieChart";
 import tesla_sales from "./tesla_sales.csv";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App" style={{ display: "flex" }}>
-                <div style={{ width: "600px"}}>
-                    <TeslaSalesPieChart data={tesla_sales} />
-                </div>
-                <div style={{ marginLeft: "50px" }}>
-                    <h2 style={{ textAlign: "center", marginLeft: "-170px" }}>Model Proportion</h2>
-                    <ModelSalesPieChart />
-                </div>
-                <div>
-                    <h2>Wheels Proportion</h2>
-                    <WheelSalesPieChart />
-                </div>
-                <div
-                    id="tooltip"
-                    style={{
-                        position: "absolute",
-                        opacity: 0,
-                        pointerEvents: "none",
-                        background: "white",
-                        border: "1px solid #ccc",
-                        borderRadius: "5px",
-                        padding: "10px",
-                        boxShadow: "0px 4px 10px rgba(0,0,0,0.2)",
-                    }}
-                >
-                    <svg></svg>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App" style={{ display: "flex" }}>
+        <div style={{ width: "600px" }}>
+          <TeslaSalesPieChart data={tesla_sales} />
+        </div>
+
+        <div style={{ marginLeft: "50px" }}>
+          <h2 style={{ textAlign: "center", marginLeft: "-170px" }}>
+            Model Proportion
+          </h2>
+          <ModelSalesPieChart />
+        </div>
+
+        <div>
+          <h2>Wheels Proportion</h2>
+          <WheelSalesPieChart />
+        </div>
+        
+        <div
+          id="tooltip"
+          style={{
+            position: "absolute",
+            opacity: 0,
+            pointerEvents: "none",
+            background: "white",
+            border: "1px solid #ccc",
+            borderRadius: "5px",
+            padding: "10px",
+            boxShadow: "0px 4px 10px rgba(0,0,0,0.2)",
+          }}
+        >
+          <svg></svg>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
